a758a78e0c0e136d253badd7b407101ab8356942
Federico Grandi <fgrandi30@gmail.com>
2018-01-01 23:04:07 +0100

    Add example 1

---
+++ examples/001_modules/app.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+
+const file = require("./file.js");
+
+client.login(config.token);
+
+client.on("error", (e) => console.error(e));
+client.on("warn", (w) => console.warn(w));
+client.on("debug", (d) => console.info(d));
+
+client.on("ready", () => {
+  let exp;
+  if (file.likesGoT) exp = "It does.";
+  else exp = "It doesn't.";
+  module.exports = {
+    exp,
+    likesIt: file.likesGoT
+  };
+  module.exports.returnedV = file.run();
+});
---
+++ examples/001_modules/file.js
@@ -0,0 +1,7 @@
+const app = require("./app.js");
+
+module.exports.likesGoT = true;
+module.exports.run = function() {
+  console.log(app.exp);
+  return "Hey";
+};
